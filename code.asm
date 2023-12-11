570f0aea689e977cad9cb4ec6722b3b333f4a9e7
ajpaneru <rojanpaneru0002@gmail.com>
2023-12-10 21:36:26 -0500

    Set up main program entry and jump to input loop

--- code.asm
+++ code.asm
@@ -2,3 +2,7 @@
 prompt:             .asciiz "Enter an integer (N >= 25): "
 error_display:      .asciiz "Illegal Number! Please Write valid numbers i.e (N >= 25) \n"
 fib_display:        .asciiz "Fibonacci Sequence:\n"
+
+.text
+main:
+    j input_loop
