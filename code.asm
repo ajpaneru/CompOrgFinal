160c5ce5de6cb6a6c3a9866d7ddbb71725ea5975
Sameer Acharya <acharyasameer2002@gmail.com>
2023-12-10 21:25:00 -0500

    Initialize data section with prompts and error messages

---
+++ code.asm
@@ -0,0 +1,4 @@
+.data
+prompt:             .asciiz "Enter an integer (N >= 25): "
+error_display:      .asciiz "Illegal Number! Please Write valid numbers i.e (N >= 25) \n"
+fib_display:        .asciiz "Fibonacci Sequence:\n"
