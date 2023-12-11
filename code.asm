c106e70bbb8fceb123453dfe1515062461eae2e6
ajpaneru <rojanpaneru0002@gmail.com>
2023-12-10 21:38:41 -0500

    Implement loop for calculating Fibonacci sequence

--- code.asm
+++ code.asm
@@ -31,3 +31,21 @@ fibonacci_sequence:
     li $v0, 4
     la $a0, fib_display
     syscall
+
+
+fibonacci_loop:
+    li $v0, 1
+    move $a0, $t1
+    syscall
+
+    add $t4, $t1, $t2
+    move $t1, $t2
+    move $t2, $t4
+    addi $t3, $t3, 1
+
+    li $v0, 11
+    li $a0, 32
+    syscall
+
+    bge $t3, $t0, exit_code
+    j fibonacci_loop
