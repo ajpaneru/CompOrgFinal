50263076b0b2163eb3f32444a1905660475b21e4
ajpaneru <rojanpaneru0002@gmail.com>
2023-12-10 21:57:35 -0500

    updating comments in the code

--- code.asm
+++ code.asm
@@ -1,60 +1,58 @@
 .data
 prompt:             .asciiz "Enter an integer (N >= 25): "
-error_display:      .asciiz "Illegal Number! Please Write valid numbers i.e (N >= 25) \n"
+error_display:      .asciiz "Invalid Number! Please enter N >= 25\n"
 fib_display:        .asciiz "Fibonacci Sequence:\n"
 
 .text
 main:
-    j input_loop
+    j input_loop     # Jump to input loop
 
 input_loop:
-    li $v0, 4
+    li $v0, 4         # Print prompt
     la $a0, prompt
     syscall
 
-    li $v0, 5
+    li $v0, 5         # Read an integer
     syscall
     move $t0, $v0
 
-    bge $t0, 25, fibonacci_sequence
+    bge $t0, 25, fibonacci_sequence  # Check if N >= 25
 
-    li $v0, 4
+    li $v0, 4         # Print error message
     la $a0, error_display
     syscall
     j input_loop
 
 fibonacci_sequence:
-    li $t1, 0
+    li $t1, 0         # Initialize Fibonacci variables
     li $t2, 1
     li $t3, 0
 
-    li $v0, 4
+    li $v0, 4         # Print Fibonacci display message
     la $a0, fib_display
     syscall
 
-
 fibonacci_loop:
-    li $v0, 1
+    li $v0, 1         # Print current Fibonacci number
     move $a0, $t1
     syscall
 
-    add $t4, $t1, $t2
+    add $t4, $t1, $t2  # Calculate next Fibonacci number
     move $t1, $t2
     move $t2, $t4
     addi $t3, $t3, 1
 
-    li $v0, 11
+    li $v0, 11        # Print a space
     li $a0, 32
     syscall
 
-    bge $t3, $t0, exit_code
+    bge $t3, $t0, exit_code  # Exit loop if done
     j fibonacci_loop
 
-
 exit_code:
-    li $v0, 1
+    li $v0, 1         # Print final Fibonacci number
     move $a0, $t1
     syscall
 
-    li $v0, 10
+    li $v0, 10        # Exit program
     syscall
